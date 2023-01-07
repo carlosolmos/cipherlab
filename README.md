c82da400ff8b2307bbeaeeec673f2504660c9807
carlos <olmos.carlos@gmail.com>
2023-01-07 10:52:03 -0800

    better documentation

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # cipherlab
 
-Examples and exercises to learn about ciphers.
+Examples and exercises to learn about encryption.
 
-The first batch of exercises are based on the work of Simon Singh in "The Code Book":
+I started this project to help me understand the encryption techniques Simon Singh explains in his fantastic book 
+[The Code Book - The Science of Secrecy From Ancient Egypt to Quantum Cryptography (Anchor Books, 2000)](https://en.wikipedia.org/wiki/The_Code_Book). 
+I plan to work with the concepts of cryptography and cryptanalysis history laid out by Singh in his book by implementing 
+the algorithms, to the best of my abilities, with small Python snippets and libraries. The purpose of this site is entirely 
+educational and mainly for my benefit. But I do hope that someone else can find it a helpful companion for their journey 
+into the world of secrets.
 
-> Singh, Simon, The Code Book: The Science of Secrecy From Ancient Egypt to Quantum Cryptography. New York, Anchor Books, 2000. Singh, Simon. The Code Book: The Science of Secrecy From Ancient Egypt to Quantum Cryptography.
+>The book: 
+> Singh, Simon, The Code Book: The Science of Secrecy From Ancient Egypt to Quantum Cryptography. New York, Anchor Books, 2000.
 
-The site is build using [streamlit.io](https://streamlit.io)
+Made with [streamlit.io](https://streamlit.io)
--- home.py
+++ home.py
@@ -1,21 +1,22 @@
 """
-Cryptography techniques
-StreamLit application
+Cryptography Lab
 """
 
 import streamlit as st
 
-st.title("Cryptography Techniques")
+st.set_page_config(page_title="Cryptography Lab by Carlos Olmos")
+st.header("Cryptography Lab")
 
 st.markdown("""
-I started this project to get a better understanding of the encryption techniques explained by Simon Singh in his \
-fantastic book The Code Book (Anchor Books, 1999).
+I started this project to help me understand the encryption techniques Simon Singh explains in his fantastic book \
+[The Code Book - The Science of Secrecy From Ancient Egypt to Quantum Cryptography (Anchor Books, 2000)](https://en.wikipedia.org/wiki/The_Code_Book). 
+I plan to work with the concepts of cryptography and cryptanalysis history laid out by Singh in his book by implementing \
+the algorithms, to the best of my abilities, with small Python snippets and libraries. The purpose of this site is entirely \
+educational and mainly for my benefit. But I do hope that someone else can find it a helpful companion for their journey \
+into the world of secrets.
 """)
 
+st.markdown("""Carlos Olmos
 
-st.write("Carlos Olmos")
-st.markdown("[carlosolmos.dev](https://carlosolmos.dev)")
+[carlosolmos.dev](https://carlosolmos.dev)""")
 
-st.markdown("""
-Powered by [streamlit](https://docs.streamlit.io/library/get-started)
-""")
